266028ed88d800511f76cd916ca333baf27a34cf
cyrdodi <cyrillusdodi@gmail.com>
2020-05-20 10:29:58 +0700

    firist commmit

---
+++ application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('no direct access script allowed');
+
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['judul'] = 'CYRDODI';
+        $this->load->view('templates/header', $data);
+        $this->load->view('home/index');
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/views/home/index.php
@@ -0,0 +1,21 @@
+<style>
+</style>
+<div class="container mt-5" style="height: 100%">
+    <div class="row text-center mt-5">
+        <div class="col mt-5">
+            <div class="vertical-center">
+                <h1 class="mt-5">CYRDODI</h1>
+                <img src="<?= base_url('assets/img/big.jpg') ?>" class="img-responsive" style="max-width: 600px">
+                <div class="row mt-2">
+                    <div class="col">
+                        <a href="" class="text-dark"><i class="lab la-github la-2x"></i></a>
+                        <a href="" class="text-dark"><i class="lab la-instagram la-2x"></i></a>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
